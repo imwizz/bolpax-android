2ce90a4d6389790c8a3556ad3fa8f42083680920
sangbas <sangaji@imwizz.co.id>
2016-01-07 10:24:56 +0700

    init commit

---
+++ app/src/main/java/id/co/imwizz/bolpax/ui/view/MainActivity.java
@@ -0,0 +1,15 @@
+package id.co.imwizz.bolpax.ui.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import id.co.imwizz.bolpax.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
