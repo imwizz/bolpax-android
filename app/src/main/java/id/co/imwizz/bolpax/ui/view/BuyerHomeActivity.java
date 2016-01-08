877b29354725ccd35b1dbfd0f5952789aa1ee97d
bimo <bimo@imwizz.co.id>
2016-01-08 15:10:28 +0700

    add layout register and buyer home

---
+++ app/src/main/java/id/co/imwizz/bolpax/ui/view/BuyerHomeActivity.java
@@ -0,0 +1,18 @@
+package id.co.imwizz.bolpax.ui.view;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import id.co.imwizz.bolpax.R;
+
+/**
+ * Created by User on 08/01/2016.
+ */
+public class BuyerHomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buyer_home);
+    }
+}
