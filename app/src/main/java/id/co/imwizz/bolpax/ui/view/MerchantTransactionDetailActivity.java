69e3cf31ca9820f4c17d2bff5b1c5f577957f3bd
bimosektiw <bimo@imwizz.co.id>
2016-02-27 09:54:11 +0700

    update merchant button reply

--- app/src/main/java/id/co/imwizz/bolpax/ui/view/MerchantTransactionDetailActivity.java
+++ app/src/main/java/id/co/imwizz/bolpax/ui/view/MerchantTransactionDetailActivity.java
@@ -13,6 +13,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -46,6 +47,8 @@ public class MerchantTransactionDetailActivity extends AppCompatActivity {
     @Bind(R.id.list_detail) ListView trxDetailText;
     @Bind(R.id.reply) Button reply;
     @Bind(R.id.toolbar) Toolbar toolbar;
+    @Bind(R.id.progressBar)
+    ProgressBar progressBar;
     Long  userid,merchantid,bolpax;
     String token,trxId;
     long trxid;
@@ -101,7 +104,8 @@ public class MerchantTransactionDetailActivity extends AppCompatActivity {
                     reply.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View v) {
-                            Toast.makeText(getBaseContext(), "ditekan", Toast.LENGTH_LONG).show();
+                            progressBar.setVisibility(View.VISIBLE);
+                            reply.setVisibility(View.GONE);
                             AddHistoryTrxBolpax addHistoryTrxBolpax = new AddHistoryTrxBolpax();
                             addHistoryTrxBolpax.setTrxId(trxId);
                             Id id = new Id();
@@ -112,6 +116,7 @@ public class MerchantTransactionDetailActivity extends AppCompatActivity {
                             RestClient.getBolpax().postAddHistoryTransaction(addHistoryTrxBolpax, new Callback<String>() {
                                 @Override
                                 public void success(String s, Response response) {
+                                    progressBar.setVisibility(View.GONE);
                                     refreshHistory();
                                 }
 
